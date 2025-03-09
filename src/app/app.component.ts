0f07bd455d2ede8011c373647a29015ed156ddec
MatheusBalbino1 <matheus.balbino@timeenergy.com.br>
2025-03-09 19:15:55 -0300

    feat: add routes

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,38 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { NavigationEnd, Router, RouterOutlet } from '@angular/router';
+import { SidebarComponent } from './components/sidebar/sidebar.component';
+import { HeaderComponent } from './components/header/header.component';
+import { CommonModule } from '@angular/common';
+import { filter } from 'rxjs';
+import { ToastModule } from 'primeng/toast';
+import { ClientService } from './services/client.service';
+import { HttpClientModule } from '@angular/common/http';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [
+    RouterOutlet,
+    SidebarComponent,
+    HeaderComponent,
+    CommonModule,
+    ToastModule,
+    HttpClientModule,
+  ],
+  providers: [ClientService],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+  styleUrl: './app.component.scss',
 })
 export class AppComponent {
   title = 'host';
+  isLoginPage = true;
+
+  constructor(private router: Router, private clientService: ClientService) {
+    this.router.events
+      .pipe(filter((event) => event instanceof NavigationEnd))
+      .subscribe((event: NavigationEnd) => {
+        this.isLoginPage = event.urlAfterRedirects.includes('login');
+      });
+  }
+
+  ngOnInit() {}
 }
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,28 @@
+import { loadRemoteModule } from '@angular-architects/module-federation';
 import { Routes } from '@angular/router';
+import { authGuard } from './guards/auth.guard';
+import { LoginComponent } from './pages/login/login.component';
+import { ListClientsComponent } from './pages/list-clients/list-clients.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: '',
+    pathMatch: 'full',
+    redirectTo: 'login',
+  },
+  {
+    path: 'login',
+    canActivate: [authGuard],
+    component: LoginComponent,
+  },
+  {
+    path: 'clients',
+    canActivate: [authGuard],
+    component: ListClientsComponent,
+  },
+  {
+    path: 'clients/selected',
+    canActivate: [authGuard],
+    component: ListClientsComponent,
+  },
+];
